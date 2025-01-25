23899341030ef2c7247e721577611047901f375f
Dick Chesterwood <richard@inceptiontraining.co.uk>
2025-01-25 18:56:33 +0000

    adding early version of Angular 19 edition

--- microservice-source-code/k8s-fleetman-webapp-angular/src/app/app.module.ts
+++ microservice-source-code/k8s-fleetman-webapp-angular/src/app/app.module.ts
@@ -1,30 +1,19 @@
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Inject } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule, Inject } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { VehicleService } from './vehicle.service';
 
-import { HttpClientModule }    from '@angular/common/http';
-import { MapComponent } from './map/map.component';
+//import { HttpClientModule }    from '@angular/common/http';
+//import { MapComponent } from './map/map.component';
 
-import { LeafletModule } from '@bluehalo/ngx-leaflet';
+//import { LeafletModule } from '@bluehalo/ngx-leaflet';
 
-import { HeaderComponent } from './header/header.component';
+//import { HeaderComponent } from './header/header.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    LeafletModule,
-    NgbModule,
-    HeaderComponent
-  ],
-  providers: [VehicleService],
-  // not sure if this needs to move? bootstrap: [AppComponent]
-})
+ 
 export class AppModule {
 }
